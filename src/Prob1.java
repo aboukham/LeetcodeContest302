3232d7dd7c46d0041007a697c9672cbb12820be8
aabdelaziz <aboukham.abdelaziz@gmail.com>
2022-07-17 13:58:48 -0500

    add prob1

---
+++ src/Prob1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Prob1 {
+    public static int[] countPairs(Collection<Integer> values){
+        int countPairs = 0;
+        int countNonPairs = 0;
+        Iterator<Integer> iter = values.iterator();
+        while (iter.hasNext()){
+            int value = iter.next();
+            countPairs += value / 2;
+            countNonPairs += value % 2;
+        }
+        return new int[]{countPairs, countNonPairs};
+    }
+    public static int[] numberOfPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(nums[i])){
+                int value = map.get(nums[i]) + 1;
+                map.put(nums[i], value);
+            }else {
+                map.put(nums[i], 1);
+            }
+        }
+        return countPairs(map.values());
+    }
+
+    public static void main(String[] args){
+        int [] nums = {1,3,2,1,3,2,2};
+        int [] pairs = numberOfPairs(nums);
+        System.out.println(Arrays.toString(pairs));
+    }
+}
+
